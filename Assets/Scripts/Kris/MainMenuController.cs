45614dc6f676d41fba210add7d2fd67ddab80391
Kris J <KrisJohnson458@gmail.com>
2018-09-22 10:36:53 -0500

    Main Menu Skeleton
    Nothing impressive

---
+++ Assets/Scripts/Kris/MainMenuController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuController : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void StartGameObject()
+    {
+        SceneManager.LoadScene("MainGame", LoadSceneMode.Single);
+    }
+
+    public void CloseGameObject()
+    {
+        Application.Quit();
+    }
+
+    public void CreditsGameObject()
+    {
+        SceneManager.LoadScene("Credits", LoadSceneMode.Single);
+    }
+
+}
