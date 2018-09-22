306e7e432a7d94550e7a48dc4d8ad02cf6af4b0f
Michael Cronin <michaelconlycronin@gmail.com>
2018-09-22 16:59:30 -0500

    health bar

---
+++ Assets/Enemy Assets/Menu/PauseMenuManager.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PauseMenuManager : MonoBehaviour {
+
+   // public static PauseMenuManager instance;
+
+    public RectTransform pauseMenuRoot;
+
+    private void Awake()
+    {
+       // if (instance == null) { instance = this; }
+        ResumeGame();
+    }
+
+    private void PauseGame()
+    {
+        pauseMenuRoot.gameObject.SetActive(true);
+        Time.timeScale = 0;
+    }
+
+    public void ResumeGame()
+    {
+        
+        pauseMenuRoot.gameObject.SetActive(false);
+        Time.timeScale = 1;
+        //PlayerController.instance.paused = false;
+
+    }
+
+	public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+   
+
+    private void Update()
+    {
+        if (Input.GetButtonDown("Cancel"))
+        {
+            if (!pauseMenuRoot.gameObject.activeSelf)
+            {
+                PauseGame();
+               // PlayerController.instance.paused = true;
+            }
+            else
+            {
+                ResumeGame();
+                // PlayerController.instance.paused = false;
+                
+            }
+        }
+        }
+}
