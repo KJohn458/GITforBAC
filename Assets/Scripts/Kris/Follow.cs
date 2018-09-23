fb459e8ff8e9fd07634adf754d8d870843bfb76e
Kris J <KrisJohnson458@gmail.com>
2018-09-22 19:55:51 -0500

    Camera

---
+++ Assets/Scripts/Kris/Follow.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Follow : MonoBehaviour {
+
+    public GameObject objToFollow;
+
+    public float speedOfFOllow = 2.0f;
+
+	
+	// Update is called once per frame
+	void Update () {
+        float interpolation = speedOfFOllow * Time.deltaTime;
+
+        Vector3 position = this.transform.position;
+        position.y = Mathf.Lerp(this.transform.position.y, objToFollow.transform.position.y, interpolation);
+        position.x = Mathf.Lerp(this.transform.position.x, objToFollow.transform.position.x, interpolation);
+
+        this.transform.position = position; 
+	}
+}
